82942868862b05a7f1d5fe944cda3d6b4d13a28c
ingenarel <ingenarelitems@gmail.com>
2024-08-13 07:52:49 +0600

    added a custom linemode!

--- init.lua
+++ init.lua
@@ -1,1 +1,16 @@
 require("omp"):setup()
+
+function Linemode:size_and_mtime()
+	local year = os.date("%Y")
+	local time = (self._file.cha.modified or 0) // 1
+
+	-- if time > 0 and os.date("%Y", time) == year then
+	-- 	time = os.date("%b %d %H:%M", time)
+	-- else
+	-- 	time = time and os.date("%b %d  %Y", time) or ""
+	-- end
+    time = os.date("%x-%I:%M:%S%p", time)
+
+	local size = self._file:size()
+	return ui.Line(string.format("%s|%s", size and ya.readable_size(size) or "-", time))
+end
